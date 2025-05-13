515a38dde2e6233455091afb4638ec068eff71fa
Viku4780 <vikrantmom5748@gmail.com>
2025-05-13 10:39:39 +0000

    [LeetCode Sync] Runtime - 2 ms (46.84%), Memory - 56.5 MB (15.58%)

---
+++ my-folder/0014-longest-common-prefix/solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) return "";
+
+  let commonPrefix = "";
+  // iterate over each character index in the first string
+  for (let i = 0; i < strs[0].length; i++) {
+    const ch = strs[0][i];
+    // reset arr for *this* character position
+    const arr = [];
+
+    // collect any string that has the same character here
+    strs.forEach(s => {
+      if (s[i] === ch) arr.push(s);
+    });
+
+    // if all strings matched at position i, extend the prefix
+    if (arr.length === strs.length) {
+      commonPrefix += ch;
+    } else {
+      // as soon as one string mismatches, stop
+      break;
+    }
+  }
+
+  return commonPrefix;
+};
+
