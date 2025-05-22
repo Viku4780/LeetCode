59a75896d970332b8eada59b90a1311d48e740ae
Viku4780 <vikrantmom5748@gmail.com>
2025-05-22 10:35:37 +0000

    [LeetCode Sync] Runtime - 33 ms (31.28%), Memory - 54.9 MB (53.50%)

--- my-folder/0001-two-sum/solution.js
+++ my-folder/0001-two-sum/solution.js
@@ -9,16 +9,12 @@ var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
         const rElement = target - nums[i];
         
-        for(let j = 0;j < nums.length;j++){
-
-            if(rElement === nums[j] && i !== j){
+        for(let j = i + 1;j < nums.length;j++){
+            if(rElement === nums[j]){
                 return [i,j];
             }
         }
-       
-    }
-   
+    }  
 };
 
-twoSum([2,7,11,15],9);
 
