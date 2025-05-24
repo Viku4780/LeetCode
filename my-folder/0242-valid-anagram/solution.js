5849a1a467534323cdeac074b567864b35ada771
Viku4780 <vikrantmom5748@gmail.com>
2025-05-24 10:27:33 +0000

    [LeetCode Sync] Runtime - 29 ms (17.55%), Memory - 56.1 MB (66.82%)

---
+++ my-folder/0242-valid-anagram/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    else{
+        const sStr = s.split('')
+                      .sort()
+                      .join('');
+
+        const tStr = t.split('')
+                      .sort()
+                      .join('');
+
+        if(sStr === tStr) return true;
+        else              return false;
+    }
+};
