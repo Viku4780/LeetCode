05308c726204b81c2a85aa575a90edf2b779e149
Viku4780 <vikrantmom5748@gmail.com>
2025-06-01 10:49:56 +0000

    [LeetCode Sync] Runtime - 17 ms (93.86%), Memory - 74.2 MB (92.22%)

---
+++ my-folder/0217-contains-duplicate/solution.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = new Set(nums);
+    if(set.size === nums.length) return false;
+    else   return true;
+};
