ec4298e39a25c0f3cb7d1fa56d72f605eb42d228
Viku4780 <vikrantmom5748@gmail.com>
2025-06-11 11:21:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 52.3 MB (97.86%)

---
+++ my-folder/0033-search-in-rotated-sorted-array/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if(nums.indexOf(target) === undefined) return -1;
+
+    return nums.indexOf(target);
+};
