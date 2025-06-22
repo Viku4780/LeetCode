30a3350cb2aa6e57313cca9717fc55d63d43adcb
Viku4780 <vikrantmom5748@gmail.com>
2025-06-22 10:52:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57.8 MB (52.49%)

---
+++ my-folder/0083-remove-duplicates-from-sorted-list/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(!head) return null;
+
+    let current = head;
+
+     while (current && current.next) {
+            if(current.val === current.next.val){
+                // Remove the next duplicate node
+            current.next = current.next.next;
+            }
+             else {
+            current = current.next;
+        }
+     }
+    return head;
+};
