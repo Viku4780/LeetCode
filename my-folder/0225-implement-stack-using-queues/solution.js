128721bb0fdc87cf01e315e8e3120a7f85b70c40
Viku4780 <vikrantmom5748@gmail.com>
2025-07-08 04:15:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.9 MB (38.44%)

---
+++ my-folder/0225-implement-stack-using-queues/solution.js
@@ -0,0 +1,51 @@
+
+var MyStack = function() {
+    this.q1 = [];
+    this.q2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+      this.q2.push(x);//enqueue to q2
+
+        //move all elements from q1 to q2
+        while (this.q1.length > 0) {
+            this.q2.push(this.q1.shift());
+        }
+
+        //swap q1 and q2
+        [this.q1, this.q2] = [this.q2, this.q1];
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.q1.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.q1[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q1.length === 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
