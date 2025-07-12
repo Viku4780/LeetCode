501bededcd32f9a4a3255d60d0ed9cb1d74b282d
Viku4780 <vikrantmom5748@gmail.com>
2025-07-12 03:56:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.3 MB (14.52%)

---
+++ my-folder/0957-minimum-add-to-make-parentheses-valid/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minAddToMakeValid = function(s) {
+    let stack = [];
+
+    for(let i=0;i<s.length;i++){
+        if(s[i] === "("){
+            stack.push("(");
+        }else if(s[i] === ")"){
+            if(stack[stack.length-1] === "("){
+                stack.pop();
+            }else{
+                stack.push(")");
+            }
+        }
+    }
+   return stack.length;  
+};
