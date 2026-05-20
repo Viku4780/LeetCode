047aa99ca4e0b6e2b50c23d4de9ef455cffcf8d7
Viku4780 <vikrantmom5748@gmail.com>
2026-05-20 12:48:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 62.3 MB (15.67%)

--- my-folder/0344-reverse-string/solution.js
+++ my-folder/0344-reverse-string/solution.js
@@ -3,9 +3,21 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    const sArr = s.slice();
-    for(let i=s.length-1;i>=0;i--){
-       s[i] = sArr[s.length-1-i];
+    // const sArr = s.slice();
+    // for(let i=s.length-1;i>=0;i--){
+    //    s[i] = sArr[s.length-1-i];
+    // }
+    // return s;
+
+    // two pointer
+    let left = 0;
+    let right = s.length - 1;
+
+    while(left<right){
+      [s[left], s[right]] = [s[right], s[left]];
+      left++;
+      right--;
     }
+
     return s;
 };
