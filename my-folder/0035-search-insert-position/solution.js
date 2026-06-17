92b3af3c640077537c73d2c1f7a88f3dd7f0c9d1
Viku4780 <vikrantmom5748@gmail.com>
2026-06-17 11:49:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54.1 MB (34.67%)

--- my-folder/0035-search-insert-position/solution.js
+++ my-folder/0035-search-insert-position/solution.js
@@ -4,17 +4,36 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  if(nums.includes(target)){
-    for(let i = 0;i < nums.length;i++){
-    const el = nums[i];
-    if(el === target) return i;
+  // if(nums.includes(target)){
+  //   for(let i = 0;i < nums.length;i++){
+  //   const el = nums[i];
+  //   if(el === target) return i;
+  // }
+  // }else{
+  //    nums.push(target);
+  //   nums.sort((a,b) => a-b);
+  //   for(let i = 0;i < nums.length ; i++){
+  //   const el = nums[i];
+  //   if(el === target) return i;
+  //   }
+  // }
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while(left<right){
+    const med = Math.floor((left + right)/2);
+
+    if(nums[med] === target) return med;
+    else if(nums[med] > target) right = med;
+    else                        left = med + 1;
   }
-  }else{
-     nums.push(target);
-    nums.sort((a,b) => a-b);
-    for(let i = 0;i < nums.length ; i++){
-    const el = nums[i];
-    if(el === target) return i;
-    }
+
+  const med = Math.floor((left + right)/2);
+
+  if(nums[med] < target) return med + 1;
+  else if(nums[med] === target) return med;
+  else {
+    return med === 0 ? 0 : med
   }
 };
