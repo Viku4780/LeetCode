2f64f4406b75425b2266421e1e3fe0976ca25a06
Viku4780 <vikrantmom5748@gmail.com>
2026-06-18 12:00:17 +0000

    [LeetCode Sync] Runtime - 16 ms (20.46%), Memory - 56 MB (73.40%)

--- my-folder/0268-missing-number/solution.js
+++ my-folder/0268-missing-number/solution.js
@@ -3,11 +3,27 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let sum = 0;
-    let legt = nums.length;
-    for(let i=0;i<legt;i++){
-        sum += nums[i];
+    // let sum = 0;
+    // let legt = nums.length;
+    // for(let i=0;i<legt;i++){
+    //     sum += nums[i];
+    // }
+    // return legt*(legt + 1)/2 - sum;
+
+
+    // binary search
+    let sortedArr = nums.sort((a,b) => a-b);
+
+    let start = 0;
+    let end = nums.length;
+     let med;
+
+    while(start <= end){
+      med = Math.floor((start+end)/2);
+
+      if(nums[med]=== med) start = med + 1;
+      else                 end = med - 1;
     }
 
-    return legt*(legt + 1)/2 - sum;
+    return start;
 };
