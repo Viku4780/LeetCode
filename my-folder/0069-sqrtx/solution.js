69b287d1a041f8776a3b8cf344be59f1b1fb49e5
Viku4780 <vikrantmom5748@gmail.com>
2026-06-18 11:32:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56.3 MB (34.88%)

--- my-folder/0069-sqrtx/solution.js
+++ my-folder/0069-sqrtx/solution.js
@@ -3,5 +3,37 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.abs(Math.floor(Math.pow(x,0.5)));
+    // return Math.abs(Math.floor(Math.pow(x,0.5)));
+
+
+     // pure square root logic 'Repeated Subtraction Method of Square Root'
+    // let count = 0;
+
+    // let result = x;
+
+    // while(result>0){
+    //   result = result - (2*count + 1);
+    //   count++;
+    // }
+
+    // return result >= 0 ? count : count - 1;
+
+
+    // binary search method
+    if(x <2) return x;
+
+    let start = 1;
+    let end = x;
+    let med ;
+
+    while(start<=end){
+      med = Math.floor((start + end)/2);
+      const sqr = med * med;
+
+      if(sqr === x) return med;
+      else if(sqr < x) start = med + 1;
+      else                   end = med - 1;
+    }
+
+    return  med * med < x ? med : med -1;
 };
