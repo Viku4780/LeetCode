97bcc083b69a4a1b754e3d911f211aaf9664cd58
Viku4780 <vikrantmom5748@gmail.com>
2026-06-19 10:22:49 +0000

    [LeetCode Sync] Runtime - 5 ms (12.70%), Memory - 56 MB (22.28%)

---
+++ my-folder/0349-intersection-of-two-arrays/solution.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    // if(nums1.length > nums2.length){
+    //   return intersection(nums2, nums1);
+    // }
+
+    // const interArr = [];
+
+    // for(let i=0; i<nums1.length; i++){
+    //   if(nums2.includes(nums1[i])){
+    //     if(interArr.includes(nums1[i])) continue;
+    //     else             interArr.push(nums1[i]);
+    //   }
+    // }
+    // return interArr;
+
+
+    // thinking of binary solution
+
+    nums2.sort((a,b) => a-b);
+
+    const result = new Set();
+
+    for(let i = 0; i < nums1.length; i++){
+      if(binarySearch(nums2, nums1[i])) result.add(nums1[i]);
+    }
+
+    return Array.from(result);
+};
+
+function binarySearch(nums2, target){
+  let start = 0;
+  let end = nums2.length - 1;
+
+  while(start <= end){
+    const med = Math.floor((start + end)/2);
+
+    if(nums2[med] === target) return true;
+    else if(nums2[med] < target) start = med + 1;
+    else                         end = med - 1;
+  }
+
+  return false;
+}
