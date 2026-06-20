83d5208b676c3c4f3b8a6d609f1cd6b1c61152aa
Viku4780 <vikrantmom5748@gmail.com>
2026-06-20 09:57:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.6 MB (57.06%)

--- my-folder/0367-valid-perfect-square/solution.js
+++ my-folder/0367-valid-perfect-square/solution.js
@@ -3,8 +3,27 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let n = Math.pow(num,0.5);
-    n = Math.ceil(n);
+    // let n = Math.pow(num,0.5);
+    // n = Math.ceil(n);
+    // return n*n === num;
 
-    return n*n === num;
+
+
+
+    // binary search method
+
+    let lower = 1;
+    let highest = num;
+
+    while(lower <= highest){
+      const med = Math.floor((lower + highest)/2);
+
+      const sqr = med * med;
+
+      if(sqr === num) return true;
+      else if(sqr < num) lower = med + 1;
+      else               highest = med - 1;
+    }
+
+    return false;
 };
