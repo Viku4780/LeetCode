142a9af33ad32ea18c55d0a29fc8c322081e6a26
Viku4780 <vikrantmom5748@gmail.com>
2026-06-23 10:40:21 +0000

    [LeetCode Sync] Runtime - 7 ms (9.10%), Memory - 54.9 MB (82.20%)

---
+++ my-folder/0167-two-sum-ii---input-array-is-sorted/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+   for(let i=0; i<=numbers.length-1;i++){
+      const rem = target - numbers[i];
+
+    // for(let j=i+1;j<=numbers.length-1;j++){
+    //   if(numbers[j] === rem) return [i+1,j+1];
+    // }
+
+      let low = i+1;
+      let hi = numbers.length - 1;
+
+      while(low <= hi){
+      const med = Math.floor((low+hi)/2);
+
+      if(numbers[med] === rem) return [i+1, med+1];
+      else if(numbers[med] < rem) low = med + 1;
+      else                        hi = med - 1;
+    }
+   }
+
+  
+};
