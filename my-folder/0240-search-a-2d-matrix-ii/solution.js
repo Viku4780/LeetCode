7a12d1e8c1330c8a87411da291356ca2ae0414fd
Viku4780 <vikrantmom5748@gmail.com>
2026-06-24 10:30:49 +0000

    [LeetCode Sync] Runtime - 2128 ms (5.77%), Memory - 57.5 MB (86.18%)

---
+++ my-folder/0240-search-a-2d-matrix-ii/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for(const row of matrix){
+      if(row[0] > target) continue;
+      
+      let low = 0;
+      let hi =  row.length-1;
+
+      while(low<=hi){
+        const med = Math.floor((low+hi)/2);
+
+        if(row[low]>target) break;
+
+        if(row[med] === target) return true;
+        else if(row[med]<target) low = med+1;
+        else if(row[med] > target) hi = med - 1;
+      }
+    }
+
+    return false
+};
