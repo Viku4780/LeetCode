f3952cb8d78669db8d69b61515d6023d787e0dc9
Viku4780 <vikrantmom5748@gmail.com>
2026-06-27 07:50:00 +0000

    [LeetCode Sync] Runtime - 2 ms (64.37%), Memory - 56.7 MB (31.09%)

--- my-folder/0167-two-sum-ii---input-array-is-sorted/solution.js
+++ my-folder/0167-two-sum-ii---input-array-is-sorted/solution.js
@@ -4,23 +4,39 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-   for(let i=0; i<=numbers.length-1;i++){
-      const rem = target - numbers[i];
+  //  for(let i=0; i<=numbers.length-1;i++){
+  //     const rem = target - numbers[i];
 
-    // for(let j=i+1;j<=numbers.length-1;j++){
-    //   if(numbers[j] === rem) return [i+1,j+1];
-    // }
+  //   // for(let j=i+1;j<=numbers.length-1;j++){
+  //   //   if(numbers[j] === rem) return [i+1,j+1];
+  //   // }
 
-      let low = i+1;
-      let hi = numbers.length - 1;
 
-      while(low <= hi){
-      const med = Math.floor((low+hi)/2);
+  //   // binary search
+  //     let low = i+1;
+  //     let hi = numbers.length - 1;
 
-      if(numbers[med] === rem) return [i+1, med+1];
-      else if(numbers[med] < rem) low = med + 1;
-      else                        hi = med - 1;
-    }
+  //     while(low <= hi){
+  //     const med = Math.floor((low+hi)/2);
+
+  //     if(numbers[med] === rem) return [i+1, med+1];
+  //     else if(numbers[med] < rem) low = med + 1;
+  //     else                        hi = med - 1;
+  //   }
+  //  }
+
+
+
+   // two pointer solution
+
+   let left = 0;
+   let right = numbers.length-1;
+
+   while(left < right){
+    const sum = numbers[left] + numbers[right];
+    if(sum === target) return [left+1, right+1];
+    else if(sum < target) left++;
+    else                  right--;
    }
 
   
