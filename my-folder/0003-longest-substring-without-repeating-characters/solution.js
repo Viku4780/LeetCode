1003f31a887f8203eb77e8c2706a5ed9e3d1a59f
Viku4780 <vikrantmom5748@gmail.com>
2026-07-18 04:05:30 +0000

    [LeetCode Sync] Runtime - 6 ms (82.19%), Memory - 55.7 MB (97.42%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.js
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.js
@@ -3,16 +3,24 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  let left = 0, maxLen = 0;
-  const seen = new Set();
+  let uni = new Set();
+  let left=0;
+  let len=0;
 
   for(let right=0;right<s.length;right++){
-    while(seen.has(s[right])){
-        seen.delete(s[left]);
-        left++;
+    if(uni.has(s[right])){
+      len = Math.max(len,right-left);
+
+      while(uni.has(s[right])){
+         uni.delete(s[left]);
+         left++;
+      }
+      uni.add(s[right]);
+    }else{
+      uni.add(s[right]);
+      len = Math.max(len, right-left+1);
     }
-    seen.add(s[right]);
-    maxLen = Math.max(maxLen,right-left+1);
   }
-  return maxLen;
+
+  return len;  
 };
