552734f33c1d0fbfa1fb0674c0d701e112ef3b74
Viku4780 <vikrantmom5748@gmail.com>
2026-08-15 07:52:00 +0000

    [LeetCode Sync] Runtime - 1 ms (16.40%), Memory - 57.2 MB (5.20%)

--- my-folder/0081-search-in-rotated-sorted-array-ii/solution.js
+++ my-folder/0081-search-in-rotated-sorted-array-ii/solution.js
@@ -18,4 +18,29 @@ var search = function(nums, target) {
     }
 
     return false;
+
+    // let low=0;
+    // let hi=nums.length-1;
+
+    // while(low<=hi){
+    //   const med = Math.floor((low+hi)/2);
+
+    //   if(nums[med]===target) return true;
+
+    //   if(nums[low]<=nums[med]){
+    //     if(nums[med]>target && nums[low]<=target){
+    //       hi=med-1;
+    //     }else{
+    //       low=med+1;
+    //     }
+    //   }else{
+    //     if(target>nums[med] && target<=nums[hi]){
+    //       low=med+1;
+    //     }else{
+    //       hi=med-1;
+    //     }
+    //   }
+    // }
+
+    // return false;
 };
