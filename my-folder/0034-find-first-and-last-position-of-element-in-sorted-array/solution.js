a58cf9af615dcea98096cd1ba0af6528fc4a88c8
Viku4780 <vikrantmom5748@gmail.com>
2026-08-15 07:46:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55 MB (34.04%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.js
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.js
@@ -4,27 +4,50 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    let firApp ;
-    let lastApp;
-    const arr = [];
+   let low=0;
+   let hi=nums.length-1;
 
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] === target){
-            firApp = i;
-            break;
-        }
+   let mid;
+
+   while(low<=hi){
+    const med = Math.floor((low+hi)/2);
+
+    if(nums[med]===target){
+      mid=med;
+      break;
     }
+    else if(nums[med]>target) hi=med-1;
+    else                      low=med+1;
+   }
 
-    for(let i=nums.length-1;i>=0;i--){
-        if(nums[i] === target){
-            lastApp = i;
-            break;
-        }
+  if(mid!==undefined){
+   let lowMid=mid;
+
+   while(low<lowMid){
+    const med = Math.floor((low+lowMid)/2);
+
+    if(nums[med]===target){
+       lowMid=med;
     }
-  if(firApp === undefined) firApp = -1;
-  if(lastApp === undefined) lastApp = -1;
+    else                      low=med+1;
+   }
+   low=lowMid;
 
-  arr.push(firApp);
-  arr.push(lastApp);
-  return arr;
+   let hiMid=mid;
+
+   while(hiMid<hi){
+    // const med = Math.floor((hi+hiMid)/2); this is the cause for the infinite loop think more
+
+    const med = Math.ceil((hiMid+hi)/2);
+
+    if(nums[med]===target){
+       hiMid=med;
+    }
+    else                      hi=med-1;
+   }
+
+   hi=hiMid;
+  }
+
+   return mid>=0 ? [low,hi] : [-1,-1];
 };
