62753bc9bbae28e0ffe33692690e6a67201b4de4
Viku4780 <vikrantmom5748@gmail.com>
2026-08-15 07:19:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.5 MB (71.77%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.js
+++ my-folder/0033-search-in-rotated-sorted-array/solution.js
@@ -9,37 +9,63 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let low = 0;
-    let high = nums.length - 1;
+    // let low = 0;
+    // let high = nums.length - 1;
 
-    while (low <= high) {
-        const mid = Math.floor((low + high) / 2);
+    // while (low <= high) {
+    //     const mid = Math.floor((low + high) / 2);
 
-        // 1. Found the target
-        if (nums[mid] === target) {
-            return mid;
+    //     // 1. Found the target
+    //     if (nums[mid] === target) {
+    //         return mid;
+    //     }
+
+    //     // 2. Check if the left half is sorted
+    //     if (nums[low] <= nums[mid]) {
+    //         // Check if the target lies within the sorted left half
+    //         if (target >= nums[low] && target < nums[mid]) {
+    //             high = mid - 1; // Search left
+    //         } else {
+    //             low = mid + 1;  // Search right
+    //         }
+    //     } 
+    //     // 3. Otherwise, the right half must be sorted
+    //     else {
+    //         // Check if the target lies within the sorted right half
+    //         if (target > nums[mid] && target <= nums[high]) {
+    //             low = mid + 1;  // Search right
+    //         } else {
+    //             high = mid - 1; // Search left
+    //         }
+    //     }
+    // }
+
+    // return -1; // Target not found
+
+
+    let low=0;
+    let hi=nums.length-1;
+
+    while(low<=hi){
+      const med = Math.floor((low+hi)/2);
+
+      if(nums[med]===target) return med;
+
+      if(nums[low]<=nums[med]){
+        if(nums[med]>target && nums[low]<=target){
+          hi=med-1;
+        }else{
+          low=med+1;
         }
-
-        // 2. Check if the left half is sorted
-        if (nums[low] <= nums[mid]) {
-            // Check if the target lies within the sorted left half
-            if (target >= nums[low] && target < nums[mid]) {
-                high = mid - 1; // Search left
-            } else {
-                low = mid + 1;  // Search right
-            }
-        } 
-        // 3. Otherwise, the right half must be sorted
-        else {
-            // Check if the target lies within the sorted right half
-            if (target > nums[mid] && target <= nums[high]) {
-                low = mid + 1;  // Search right
-            } else {
-                high = mid - 1; // Search left
-            }
+      }else{
+        if(target>nums[med] && target<=nums[hi]){
+          low=med+1;
+        }else{
+          hi=med-1;
         }
+      }
     }
 
-    return -1; // Target not found
+    return -1;
 };
 
