b7dd56c20bbe6d3dd5416ae0ee1efdc6a631305c
Viku4780 <vikrantmom5748@gmail.com>
2026-08-15 08:12:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.5 MB (5.67%)

--- my-folder/0153-find-minimum-in-rotated-sorted-array/solution.js
+++ my-folder/0153-find-minimum-in-rotated-sorted-array/solution.js
@@ -6,17 +6,16 @@ var findMin = function(nums) {
     let low = 0;
     let hi = nums.length - 1;
 
-    if(nums[low] < nums[hi]) return nums[low];
+    if(nums[low] <= nums[hi]) return nums[low];
 
     while(low <= hi){
       const med = Math.floor((low + hi)/2);
 
-      if(nums[med] < nums[hi] ) hi = med;
-      else if(nums[med] > nums[hi]) low = med + 1;
-      else if(nums[med] === nums[hi])  return nums[med];
-    }
+      if(nums[low]<=nums[hi]) return nums[low];
 
-    // return nums[hi+1];
+      if(nums[low]<=nums[med]) low=med+1;
+      else                     hi=med;
+    }
 };
 
 
