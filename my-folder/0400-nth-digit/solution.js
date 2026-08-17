63468b3e8bd7abd642cb675d1bccda4a20c7d7ee
Viku4780 <vikrantmom5748@gmail.com>
2026-08-17 04:56:46 +0000

    [LeetCode Sync] Runtime - 1 ms (17.24%), Memory - 53 MB (86.21%)

--- my-folder/0400-nth-digit/solution.js
+++ my-folder/0400-nth-digit/solution.js
@@ -3,22 +3,63 @@
  * @return {number}
  */
 var findNthDigit = function(n) {
-    let digitLength=1;
-    let count=9;
-    let startNumber=1;
+    // Digit-by-Digit Construction or Radix-scale Elimination algorithm
 
-    while(n>digitLength*count){
-      n-= digitLength*count;
+    // let digitLength=1;
+    // let count=9;
+    // let startNumber=1;
 
-      digitLength++;
-      count*=10;
-      startNumber*=10;
+    // while(n>digitLength*count){
+    //   n-= digitLength*count;
+
+    //   digitLength++;
+    //   count*=10;
+    //   startNumber*=10;
+    // }
+
+    // startNumber += Math.floor((n-1)/digitLength);
+
+    // const targetString = startNumber.toString();
+    // const index = (n - 1) % digitLength;
+
+    // return parseInt(targetString[index]);
+
+
+    // binary search
+
+    const countDigits = (x) => {
+      let start = 1;
+      let digit = 1;
+      let totalCounts = 0;
+
+      while(x>=start){
+         const end = Math.min(x,start*10-1);
+
+         totalCounts+=(end-start+1)*digit;
+
+         digit++;
+         start*=10;
+      }
+
+      return totalCounts;
     }
 
-    startNumber += Math.floor((n-1)/digitLength);
+    let low=1;
+    let hi=n;
 
-    const targetString = startNumber.toString();
-    const index = (n - 1) % digitLength;
+    while(low<hi){
+      const mid = Math.floor((low+hi)/2);
 
-    return parseInt(targetString[index]);
+      if(countDigits(mid) === n){
+        const numStr = mid.toString();
+        return Number(numStr[numStr.length-1]);
+      }
+      else if(countDigits(mid)<n) low=mid+1;
+      else                        hi=mid;
+    }
+
+    const digitsBefore = countDigits(low - 1);
+    const digitIndexInNum = n - digitsBefore - 1; 
+    
+    return Number(String(low)[digitIndexInNum]);
 };
