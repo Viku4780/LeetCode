bdbd0b796cf161b6090b50becc1221f72aa2a079
Viku4780 <vikrantmom5748@gmail.com>
2026-08-18 04:19:11 +0000

    [LeetCode Sync] Runtime - 6 ms (73.53%), Memory - 70.9 MB (85.29%)

--- my-folder/0456-132-pattern/solution.js
+++ my-folder/0456-132-pattern/solution.js
@@ -3,24 +3,39 @@
  * @return {boolean}
  */
 var find132pattern = function(nums) {
+    // let nums_k = -Infinity;
+    // const stack = [];
+
+    // // Traverse from right to left
+    // for (let i = nums.length - 1; i >= 0; i--) {
+    //     // If we find an element smaller than our best nums_k, pattern is found
+    //     if (nums[i] < nums_k) {
+    //         return true;
+    //     }
+
+    //     // Maintain monotonic decreasing stack
+    //     // If current element is larger than top of stack, it can be a peak (nums_j)
+    //     while (stack.length > 0 && nums[i] > stack[stack.length - 1]) {
+    //         nums_k = stack.pop(); // The popped element becomes our valid nums_k
+    //     }
+
+    //     // Push current element as a potential future nums_k or nums_j
+    //     stack.push(nums[i]);
+    // }
+
+    // return false;
+
     let nums_k = -Infinity;
     const stack = [];
 
-    // Traverse from right to left
-    for (let i = nums.length - 1; i >= 0; i--) {
-        // If we find an element smaller than our best nums_k, pattern is found
-        if (nums[i] < nums_k) {
-            return true;
-        }
+    for(let i=nums.length-1;i>=0;i--){
+      if(nums[i]<nums_k)  return true;
 
-        // Maintain monotonic decreasing stack
-        // If current element is larger than top of stack, it can be a peak (nums_j)
-        while (stack.length > 0 && nums[i] > stack[stack.length - 1]) {
-            nums_k = stack.pop(); // The popped element becomes our valid nums_k
-        }
+      while(stack.length>0 && nums[i]>stack[stack.length-1]){
+        nums_k=stack.pop();
+      }
 
-        // Push current element as a potential future nums_k or nums_j
-        stack.push(nums[i]);
+      stack.push(nums[i]);
     }
 
     return false;
