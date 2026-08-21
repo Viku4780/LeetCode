2018fd7c3be39ca1aba21a004816df1a51427882
Viku4780 <vikrantmom5748@gmail.com>
2026-08-21 05:16:21 +0000

    [LeetCode Sync] Runtime - 5 ms (20.62%), Memory - 60.6 MB (42.13%)

--- my-folder/0283-move-zeroes/solution.js
+++ my-folder/0283-move-zeroes/solution.js
@@ -3,20 +3,32 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    const numbers = [];
-    let countZ = 0;
-   for(let i=0;i<nums.length;i++){
-    const num = nums[i];
-        if(num === 0) {
-            countZ++;
-            nums.splice(i,1);
-            i--;
-        } 
-    }
+  //   const numbers = [];
+  //   let countZ = 0;
+  //  for(let i=0;i<nums.length;i++){
+  //   const num = nums[i];
+  //       if(num === 0) {
+  //           countZ++;
+  //           nums.splice(i,1);
+  //           i--;
+  //       } 
+  //   }
    
-    for(let i=1;i<=countZ;i++){
-        nums.push(0);
-    }
+  //   for(let i=1;i<=countZ;i++){
+  //       nums.push(0);
+  //   }
 
-    return nums;
+  //   return nums;
+
+  let lef=0;
+
+  for(let ryt=0;ryt<nums.length;ryt++){
+    if(nums[ryt]=== 0){
+      if(nums[lef]===0) continue;
+      else  lef++;
+    }else{
+      [nums[lef],nums[ryt]]=[nums[ryt],nums[lef]];
+      lef++;
+    }
+  }
 };
