6a91d2dda328b40831d2ef026b9c7b86ac181d95
Viku4780 <vikrantmom5748@gmail.com>
2026-08-21 04:38:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57 MB (75.99%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.js
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.js
@@ -3,16 +3,36 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-const removeDuplicates = [];
- nums.forEach(n => {
-    if(removeDuplicates.includes(n)) return;
-    else  removeDuplicates.push(n);
- });
- nums.splice(removeDuplicates.length);
+// const removeDuplicates = [];
+//  nums.forEach(n => {
+//     if(removeDuplicates.includes(n)) return;
+//     else  removeDuplicates.push(n);
+//  });
+//  nums.splice(removeDuplicates.length);
 
- for(let i=0;i<removeDuplicates.length;i++){
-    nums[i] = removeDuplicates[i];
+//  for(let i=0;i<removeDuplicates.length;i++){
+//     nums[i] = removeDuplicates[i];
+//  }
+
+//  return nums.length;
+
+//  if(nums.length===1) return 1;
+
+ let left=0;
+ let count=1;
+
+ for(let ryt=1;ryt<nums.length;ryt++){
+   if(nums[left]===nums[ryt]){
+     continue;
+   }else{
+    if(ryt-left>1){
+      left++;
+      nums[left]=nums[ryt];
+    }else  left++;
+    count++;
+   }
  }
 
- return nums.length;
+ return count;
+ 
 };
