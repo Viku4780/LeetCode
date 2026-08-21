0b3134728bfa345adea845f6ca2798832244eaf4
Viku4780 <vikrantmom5748@gmail.com>
2026-08-21 05:23:55 +0000

    [LeetCode Sync] Runtime - 4 ms (89.00%), Memory - 59.4 MB (80.18%)

--- my-folder/0345-reverse-vowels-of-a-string/solution.js
+++ my-folder/0345-reverse-vowels-of-a-string/solution.js
@@ -3,28 +3,25 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    const setOfVowel = ['a','e','i','o','u','A','E','I','O','U'];
-    const arr = s.split('')
-    const vowelCont = [];
+    const set = new Set(['a','e','i','o','u','A','E','I','O','U']);
+    const arr = s.split('');
 
-    for(let i=0;i<s.length;i++){
-        const apl = s[i];
-        if(setOfVowel.includes(apl)){
-            vowelCont.push(apl);
-        }
+    let lef=0;
+    let ryt=arr.length-1;
+
+    while(lef<ryt){
+      if(set.has(arr[lef]) && set.has(arr[ryt])){
+        [arr[lef],arr[ryt]] = [arr[ryt],arr[lef]];
+        lef++;
+        ryt--;
+      }
+      else if(!set.has(arr[lef])) lef++;
+      else if(!set.has(arr[ryt])) ryt--;
+      else{
+        lef++;
+        ryt--;
+      }
     }
 
-    vowelCont.reverse();
-    let index = 0;
-    
-    for(let i=0;i<s.length;i++){
-        const apl = s[i];
-        if(setOfVowel.includes(apl)){
-            arr[i] = vowelCont[index];
-            index++;
-        }
-    }
-    s = arr.join("");
-
-    return s;
+    return arr.join('');
 };
