c910af768df1e6c46001c6d13ceb535853cd67aa
Viku4780 <vikrantmom5748@gmail.com>
2026-08-21 04:42:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54.1 MB (18.43%)

--- my-folder/0027-remove-element/solution.js
+++ my-folder/0027-remove-element/solution.js
@@ -4,12 +4,23 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let k = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] !== val){
-            nums[k] = nums[i];
-            k++;
-        }
+    // let k = 0;
+    // for(let i = 0; i < nums.length; i++){
+    //     if(nums[i] !== val){
+    //         nums[k] = nums[i];
+    //         k++;
+    //     }
+    // }
+    // return k;
+
+    let lef = 0;
+
+    for(let ryt=0;ryt<nums.length;ryt++){
+      if(nums[ryt]!==val){
+        nums[lef]=nums[ryt];
+        lef++;
+      }
     }
-    return k;
+
+    return lef;
 };
