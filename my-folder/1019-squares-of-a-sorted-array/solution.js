5ca0d785c90bee965def87f0fdf8b86477324be1
Viku4780 <vikrantmom5748@gmail.com>
2026-08-22 04:41:41 +0000

    [LeetCode Sync] Runtime - 6 ms (57.78%), Memory - 63.3 MB (6.24%)

--- my-folder/1019-squares-of-a-sorted-array/solution.js
+++ my-folder/1019-squares-of-a-sorted-array/solution.js
@@ -3,6 +3,26 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    const sqrNums = nums.map(n => n*n);
-    return sqrNums.sort((a,b) => a-b);
+    // const sqrNums = nums.map(n => n*n);
+    // return sqrNums.sort((a,b) => a-b);
+
+    let ryt=nums.length-1;
+    let left=0;
+    const n = nums.length;
+
+    const result = new Array(n);
+    let p=n-1;
+
+    while(left<=ryt){
+      if(nums[left]*nums[left]<=nums[ryt]*nums[ryt]){
+        result[p]=nums[ryt]*nums[ryt];
+        ryt--;
+      }else{
+        result[p]=nums[left]*nums[left];
+        left++;
+      }
+      p--;
+    }
+
+    return result;
 };
