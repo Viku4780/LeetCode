dc0501e993cf86f3809da751ad637b08a7209eff
Viku4780 <vikrantmom5748@gmail.com>
2026-08-22 03:52:27 +0000

    [LeetCode Sync] Runtime - 3 ms (25.74%), Memory - 53.1 MB (97.43%)

--- my-folder/0349-intersection-of-two-arrays/solution.js
+++ my-folder/0349-intersection-of-two-arrays/solution.js
@@ -19,17 +19,40 @@ var intersection = function(nums1, nums2) {
     // return interArr;
 
 
-    // thinking of binary solution
+    // binary solution
+    // nums2.sort((a,b) => a-b);
 
-    nums2.sort((a,b) => a-b);
+    // const result = new Set();
 
-    const result = new Set();
+    // for(let i = 0; i < nums1.length; i++){
+    //   if(binarySearch(nums2, nums1[i])) result.add(nums1[i]);
+    // }
 
-    for(let i = 0; i < nums1.length; i++){
-      if(binarySearch(nums2, nums1[i])) result.add(nums1[i]);
+    // return Array.from(result);
+
+
+
+    // two pointer solution
+    nums1.sort((a,b)=>a-b);
+    nums2.sort((a,b)=>a-b);
+
+    const interArr = [];
+    let i=0;
+    let j=0;
+
+    while(i<nums1.length && j<nums2.length){
+      if(nums1[i]===nums2[j]){
+        if(interArr.length===0 || interArr[interArr.length-1] !== nums1[i]){
+          interArr.push(nums1[i]);
+        }
+        i++;
+        j++;
+      }
+      else if(nums1[i]<nums2[j]) i++;
+      else                        j++;
     }
 
-    return Array.from(result);
+    return interArr;
 };
 
 function binarySearch(nums2, target){
