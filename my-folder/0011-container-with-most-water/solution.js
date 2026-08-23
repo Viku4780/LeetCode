5ce599b10f7e3067127db42f3b180b09396aa3ee
Viku4780 <vikrantmom5748@gmail.com>
2026-08-23 04:22:31 +0000

    [LeetCode Sync] Runtime - 7 ms (5.64%), Memory - 62.6 MB (73.62%)

--- my-folder/0011-container-with-most-water/solution.js
+++ my-folder/0011-container-with-most-water/solution.js
@@ -3,17 +3,32 @@
  * @return {number}
  */
 var maxArea = function(height) {  
-    let left = 0;
-    let right = height.length-1;
-   let maxVal = 0;
+  //  let left = 0;
+  //  let right = height.length-1;
+  //  let maxVal = 0;
 
-        while(left<right){
-            const area = Math.min(height[left],height[right])*(right-left);
-            maxVal = Math.max(area,maxVal);
+  //       while(left<right){
+  //           const area = Math.min(height[left],height[right])*(right-left);
+  //           maxVal = Math.max(area,maxVal);
             
-            if(height[left]<height[right]) left++;
-            else          right--;
-        }
+  //           if(height[left]<height[right]) left++;
+  //           else          right--;
+  //       }
     
-    return maxVal;
+  //   return maxVal;
+
+  let maxWater=0;
+
+  let left=0;
+  let right=height.length-1;
+
+  while(left<right){
+    let val = (right-left)*Math.min(height[left],height[right]);
+    maxWater = Math.max(maxWater,val);
+
+    if(height[left]<height[right]) left++;
+    else                           right--;
+  }
+
+  return maxWater;
 };
