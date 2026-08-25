c9809bd271adda4b25c9a019085b704da7dc1915
Viku4780 <vikrantmom5748@gmail.com>
2026-08-25 06:20:36 +0000

    [LeetCode Sync] Runtime - 34 ms (19.11%), Memory - 86.2 MB (5.16%)

--- my-folder/0287-find-the-duplicate-number/solution.js
+++ my-folder/0287-find-the-duplicate-number/solution.js
@@ -3,38 +3,40 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    // const set = new Set();
-    // let start = 0;
-    // let end = nums.length-1;
+    const set = new Set();
+    let start = 0;
+    let end = nums.length-1;
 
-    // while(start<end){
-    //     if(set.has(nums[start])) return nums[start];
-    //     set.add(nums[start]);
+    while(start<=end){
+        if(set.has(nums[start])) return nums[start];
+        set.add(nums[start]);
 
-    //     if(set.has(nums[end])) return nums[end];
-    //     set.add(nums[end]);
+        if(set.has(nums[end])) return nums[end];
+        set.add(nums[end]);
         
-    //     start++;
-    //     end--;
+        start++;
+        end--;
         
-    //     if(set.has(nums[Math.floor(nums.length/2)])) return nums[Math.floor(nums.length/2)];
-    // }
-
-    let low = 1;
-    let hi = nums.length-1;
-
-    while(low<hi){
-      const mid = Math.floor((low+hi)/2);
-
-      let count=0;
-
-      for(const num of nums){
-        if(num<=mid) count++;
-      }
-
-      if(count>mid) hi=mid;
-      else          low=mid+1;
+        // if(set.has(nums[Math.floor(nums.length/2)])) return nums[Math.floor(nums.length/2)];
     }
 
-    return low;
+
+    // binary search
+    // let low = 1;
+    // let hi = nums.length-1;
+
+    // while(low<hi){
+    //   const mid = Math.floor((low+hi)/2);
+
+    //   let count=0;
+
+    //   for(const num of nums){
+    //     if(num<=mid) count++;
+    //   }
+
+    //   if(count>mid) hi=mid;
+    //   else          low=mid+1;
+    // }
+
+    // return low;
 };
