ae497370bc649afe8e71db08e5dcc1b45f1a3c1e
Viku4780 <vikrantmom5748@gmail.com>
2026-08-30 04:42:48 +0000

    [LeetCode Sync] Runtime - 21 ms (63.55%), Memory - 58.9 MB (50.47%)

--- my-folder/0395-longest-substring-with-at-least-k-repeating-characters/solution.js
+++ my-folder/0395-longest-substring-with-at-least-k-repeating-characters/solution.js
@@ -4,35 +4,8 @@
  * @return {number}
  */
 var longestSubstring = function(s, k) {
-    // const map = new Map();
-    // let maxUnique=0;
-    // let left=0;
-    // let len=0;
-
-    // for(let i=0;i<s.length;i++){
-    //   if(!map.has(s[i])){
-    //     maxUnique++;
-    //   }else{
-    //     map.set(s[i],1);
-    //   }
-    // }
-
-    // for(let currentUnique=1; currentUnique<=maxUnique; currentUnique++){
-    //     let uniqueCnt=0;
-    //     const mapNew= new Map();
-
-    //     for(let right=0; right<s.length; right++){
-    //       if(mapNew.has(s[right])){
-
-    //       }
-    //     }
-    // }
-
-
-
-
     // sliding window
-     let maxLen = 0;
+    let maxLen = 0;
     
     // Find total unique characters in the string to optimize the loops
     const totalUnique = new Set(s).size;
@@ -77,5 +50,4 @@ var longestSubstring = function(s, k) {
     }
     
     return maxLen;
-
 };
